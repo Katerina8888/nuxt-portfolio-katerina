d8e7f48f838fb314814807844cc5830f31ea1ac4
katerina <katerina@igloonet.cz>
2025-02-14 17:49:21 +0100

    feat: enhance About page layout and add typewriter effect for headings

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,53 @@
 <template>
-  <div>
-    tadadddd
+  <div class="p-8 ml-32 md:w-1/2 lg:w-1/3">
+    <h1 class="text-6xl pb-8 typewriter pt-6">About me 🐱</h1>
+    <p class="pb-8 text-xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere maiores
+      beatae a, possimus hic doloribus animi assumenda tempora inventore
+      praesentium libero! Sint esse, ratione aperiam culpa consequatur molestiae
+      velit assumenda!
+    </p>
+    <p class="pb-8 text-xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sequi,
+      deleniti dolores saepe nihil obcaecati voluptatibus. Et quisquam labore
+      unde facilis doloremque quaerat placeat laboriosam consequuntur fugiat
+      reprehenderit sapiente exercitationem voluptates voluptas amet officia non
+      ullam obcaecati vero iure, hic illo doloribus quia perferendis eum.
+      Suscipit nisi aliquid quasi magni.
+    </p>
+    <p class="pb-8 text-xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores inventore
+      voluptatum minima est perspiciatis, veniam, optio laudantium ipsam
+      repudiandae sed, quod ducimus quasi quam! Neque distinctio repellendus
+      voluptatem non amet, similique cumque quos nihil officiis odit corrupti
+      aut? Consequatur, quasi?
+    </p>
+    <p class="pb-8 text-xl">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+      voluptas maiores delectus est sit natus? Nemo fugit quae deleniti
+      assumenda ea voluptas modi ratione atque dolore. Illo culpa doloremque
+      alias dolorum corporis rerum repellat error saepe praesentium soluta, vero
+      quis nisi expedita suscipit reiciendis quaerat earum assumenda dolores!
+      Ducimus quibusdam rem nobis itaque doloribus iusto inventore eveniet
+      eligendi adipisci ex.
+    </p>
   </div>
 </template>
 
-<script lang="ts" setup>
+<style scoped>
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
 
-</script>
+.typewriter {
+  white-space: nowrap;
+  overflow: hidden;
+  width: 0;
+  animation: typing 4s steps(40) 0.5s 1 normal both;
+}
+</style>
