1a1f3cfb8c18bbdd9be21c8f620cfeddd0412f57
katerina <katerina@igloonet.cz>
2025-02-14 14:56:16 +0100

    fonts: problem s nacitani fontu

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,29 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          Castoro: true,
+          'Markazi Text': [400, 700],
+          'Josefin+Sans': true,
+        },
+        display: 'swap',
+        prefetch: true,
+        preload: true,
+        inject: true,
+      },
+    ],
+  ],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
-  }
+  },
+  image: {
+    dir: 'src/assets/images',
+  },
 })
